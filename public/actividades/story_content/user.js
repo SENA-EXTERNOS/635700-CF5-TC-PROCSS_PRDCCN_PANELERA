7bb31333ef4363fddb735a8871a5ba12d99b94f0
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-11-17 17:20:13 -0500

    Tema 1 AD
    Se agrega AD y se adelanta tema 1

---
+++ public/actividades/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6n4nszCs8nN":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.close();
+}
+
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -79,13 +79,242 @@
       .col-md-4(data-aos="fade-left")
         img(src='@/assets/curso/temas/T1/img-8.png', alt='mujer con un laptop y un robot')
 
+    .row.mt-5
+      .col-10.offset-1
+        .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5.mb-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/temas/T1/img-9.png')})`}"
+          )
+          .bloque-texto-g__texto.p-4
+            p.mb-0 En conclusión, la extracción de los jugos es el paso de la caña a través del molino donde se exprime la caña, con esta operación se obtiene un jugo o guarapo crudo como producto principal y bagazo húmedo (verde) que se emplea como combustible para la hornilla, para determinar el porcentaje de extracción se debe pesar el bagazo (aunque algo del peso se pierde, pues es difícil contar el peso del bagacillo porque se desecha en el jugo), (Osorio, 2007b) (Romero et al., 2009).
+
+
+    .row.mt-5
+      .col-10.offset-1
+        .titulo-sexto.color-acento-contenido.p-1.mb-5
+          .tarjeta.color-tarjetas.p-3.mb-5
+            .row.justify-content-around.align-items-center
+              .col
+                .row.justify-content-between.align-items-center
+                  .col.mb-3.mb-sm-0
+                    p.mb-4 Por ejemplo, suponiendo que se pesan 90 kg de caña, los cuales al momento de moler arrojan 40 kg de bagazo, el porcentaje de extracción se calcula
+                    p.mb-3 <b>Extracción=90 kg-39 kg 90 kg X 100= 57 %</b>
+
+    Separador
+
+
+
+    #t_1_2.titulo-segundo.color-acento-contenido
+      h2 1.2 Molino y mantenimiento
+
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-md-3(data-aos="fade-right")
+        img(src='@/assets/curso/temas/T1/img-10.svg', alt='mujer con un laptop y un robot')
+      .col-md-9
+        p.mb-5 Para llevar a cabo el proceso de extracción del jugo que contiene la sacarosa en dilución, desde siempre se han empleado diferentes utensilios metálicos y de madera para aprisionar y exprimir la caña de azúcar, estas herramientas utilizan una técnica de tres rodillos que llevan por nombre mazas y pueden ser lisos o estriados; consiste en colocar la caña en medio de las tres mazas, para hacerlas pasar y, por la acción de aplastamiento, se efectúa el rompimiento de las células que contienen la sacarosa que se libera junto con otras sustancias solubles.
+        p.mb-5 A continuación, se presentan algunas de las características de los molinos:
+
+
+    .tarjeta.color-linea-tiempo.p-4.mb-5
+      LineaTiempoC.color-acento-botones(text-small)
+        .row.justify-content-between.align-items-center(titulo="Paso 1")
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T1/img-11.png', alt='Texto que describa la imagen')
+
+
+          .col-md-6.mb-4.mb-md-0
+            p Originalmente las mazas se construían de madera y, como resultado de la evolución técnica, también se empezaron a producir en metal. Asimismo, se podían encontrar molinos en una de dos posiciones, verticales u horizontales, accionados por fuerza animal o del hombre; posteriormente, el desarrollo técnico y tecnológico posibilita la construcción de trapiches o molinos de diferentes números de mazas, hasta tal punto que hoy podemos encontrar trapiches hasta de seis mazas.
+          
+        .row.justify-content-between.align-items-center(titulo="Paso 2")
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T1/img-12.png', alt='Texto que describa la imagen')
+
+
+          .col-md-6.mb-4.mb-md-0
+            p Los molinos de madera han cumplido su ciclo y, paulatinamente, se han descontinuado de las fábricas para ser usados en trabajos de poca envergadura, donde las cantidades de extracción son pequeñas y la materia prima requerida es mínima; sin embargo, pueden seguir siendo útiles donde la tecnología está ausente por ser lugares remotos.
+
+
+        .row.justify-content-between.align-items-center(titulo="Paso 3")
+          .col-md-6
+            figure
+              .titulo-sexto.color-acento-contenido
+                p.mb-0 
+                  p.mb-1 <b>Figura 1.</b><i> Molinos</i>
+              img(src='@/assets/curso/temas/T1/img-13.png', alt='Texto que describa la imagen')
+              figcaption A. Molino vertical de tres mazas. B. Molino horizontal de tres mazas. Reproducida de Innovaciones en el trapiche panelero: la producción de panela granulada. Morales, Osorio & Rodríguez (2017). Agroproductividad, 10(11), 42.
+
+
+          .col-md-6.mb-4.mb-md-0
+            p Los trapiches de tres mazas verticales accionados por animales contribuyeron en gran medida en el inicio de la actividad panelera con fines comerciales, mejorando la producción para dar respuesta a las altas demandas del consumo de la panela. La necesidad de ampliar la producción lleva a la construcción de trapiches horizontales de tres mazas accionados por energía hidráulica, mecánica o eléctrica, aportando así, en gran medida, a la dinámica de la actividad comercial para el desarrollo del sector y del país. 
+
+        .row.justify-content-between.align-items-center(titulo="Paso 4")
+          .col-md-6
+            figure
+              .titulo-sexto.color-acento-contenido
+                p.mb-0 
+                  p.mb-1 <b>Figura 2.</b><i> Características del rendimiento del molino</i>
+              img(src='@/assets/curso/temas/T1/img-14.png', alt='Texto que describa la imagen')
+              figcaption Adaptada de (Sandoval S, s. f.)
+
+
+          .col-md-6.mb-4.mb-md-0
+            p Para atender la demanda de panela se debe tener presente la funcionalidad del equipo, donde uno de los aspectos más importantes radica en las toneladas de caña molida por hora o porcentaje de extracción de jugo por hora.
+          
+        .row.justify-content-between.align-items-center(titulo="Paso 5")
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T1/img-15.png', alt='Texto que describa la imagen')
+
+
+          .col-md-6.mb-4.mb-md-0
+            p Hoy en la mayoría de las fábricas paneleras el equipo utilizado para realizar la extracción del jugo es el molino conocido como trapiche o molino horizontal. Es una máquina de tres mazas (rodillos), por los cuales atraviesan los tallos de la caña para ser comprimidos con el fin de extraer el jugo o guarapo almacenado en la planta llamada caña de azúcar, quedando como residuo del proceso el desecho denominado bagazo, (Moreno et al., 2017).
+
+
+        .row.justify-content-between.align-items-center(titulo="Paso 6")
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T1/img-16.png', alt='Texto que describa la imagen')
+
+
+          .col-md-6.mb-4.mb-md-0
+            p La extracción se refiere a la cantidad de jugo que se extrae de la caña de azúcar. La ecuación de la extracción es:
+
+            figure
+              img(src='@/assets/curso/temas/T1/img-17.png', alt='Texto que describa la imagen')
+      
+    ol.lista-ol--cuadro.mb-3
+      li
+        .lista-ol--cuadro__vineta.listas-primera 
+          span 
+        p <b>Selección, operación y montaje de los molinos paneleros</b>
+
     
-    .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5
-      .bloque-texto-g__img(
-        :style="{'background-image': `url(${require('@/assets/curso/temas/T1/img-9.png')})`}"
-      )
-      .bloque-texto-g__texto.p-4
-        p.mb-0 En conclusión, la extracción de los jugos es el paso de la caña a través del molino donde se exprime la caña, con esta operación se obtiene un jugo o guarapo crudo como producto principal y bagazo húmedo (verde) que se emplea como combustible para la hornilla, para determinar el porcentaje de extracción se debe pesar el bagazo (aunque algo del peso se pierde, pues es difícil contar el peso del bagacillo porque se desecha en el jugo), (Osorio, 2007b) (Romero et al., 2009).
+    .row.justify-content-center.align-items-center.mb-5
+      .col-md-4(data-aos="fade-right")
+        img(src='@/assets/curso/temas/T1/img-18.png', alt='mujer con un laptop y un robot')
+      .col-md-8
+        .cajon-b.color-primario.abajo-derecha.p-1.mb-5
+          p La utilidad y la vida media de los molinos paneleros está determinada por la selección del equipo, la instalación, la manera de operar y sobre todo el mantenerlo adecuado; luego se debe determinar la utilidad del molino al expresarse en función de la capacidad de la molienda, la concentración del jugo extraído y el consumo bajo de potencia. De acuerdo con Quezada, (2007), para mejorar la extracción de un molino de tres mazas se recomienda lo siguiente:
+          ol.lista-ol--cuadro.mb-3
+            li
+              .lista-ol--cuadro__vineta.listas-segunda 
+                span 
+              p Abertura del par quebrador (maza mayal y maza recibidora) para entrada de la caña, entre 11 y 15 mm.
+
+            li
+              .lista-ol--cuadro__vineta.listas-segunda 
+                span 
+              p Abertura del par repasador (maza mayal y maza repasadora) para salida del bagazo, entre 0,4 y 1 mm.
+            
+            li
+              .lista-ol--cuadro__vineta.listas-segunda 
+                span 
+              p Velocidad de las mazas, entre 6 y 100 vueltas/minuto.
+
+
+    
+    .col-md-6.mx-auto.mb-5
+      figure
+        .titulo-sexto.color-acento-contenido
+          p.mb-0 
+            p.mb-1 <b>Figura 3.</b><i> Cómo mejorar la extracción de un molino de tres mazas</i>
+        img(src='@/assets/curso/temas/T1/img-19.png', alt='Texto que describa la imagen')
+        figcaption Nota: Reproducida de Modelación matemática de la flotación de la maza superior de los molinos de caña de azúcar. Cabello et al. (2012). 
+
+
+    .col-md-10.mx-auto.mb-5
+      .tarjeta.color-tarjetas-segunda.p-3.mb-5
+        .row.justify-content-around.align-items-center
+          .col-3.col-sm-2.col-lg-1
+            img(src="@/assets/curso/temas/T1/img-20.svg")
+          .col
+            .row.justify-content-between.align-items-center
+              .col.mb-3.mb-sm-0
+                p.text-small Para profundizar en el estudio de los molinos de tres mazas y aprender más sobre sus características y funcionamiento, lo invitamos a observar atentamente el video:
+              .col-auto.mb-3.mb-sm-0
+                a.boton.color-acento-botones.texto-blanco(href="https://www.youtube.com/watch?v=sYV_5L0AKoI" target="_blank")
+                  span  Enlace
+                  i.fas.fa-link
+
+    ol.lista-ol--cuadro.mb-3
+      li
+        .lista-ol--cuadro__vineta.listas-primera 
+          span 
+        p <b>Selección del molino panelero</b>
+
+
+    .scroll-x-tabla
+      .col-10.offset-2.bg-amarillo-claro.borde-der-secundario-19.px-5.py-4.mt-5
+        .row.mt-5
+        .col-10.px-5.py-4
+          .row
+            .col-12.d-lg-block
+              .bloque-texto-x.fcb.p-3.p-sm-4.p-md-5.mb-5
+                .bloque-texto-x__img(data-aos='fade-right')(
+                  :style="{'background-image': `url(${require('@/assets/curso/temas/T1/img-21.png')})`}"
+                )
+                .bloque-texto-x__texto.p-4
+                  p.mb-5 La capacidad de la molienda es la característica más importante a tener en cuenta, pues debe estar disponible para ser utilizada con la cantidad de caña que llega al trapiche, centrándose en la capacidad del molino respecto a las dimensiones de las mazas, las características de tamaño, potencia del motor y las especificaciones de la correa. El equipo de molienda se debe manejar de forma óptima para lograr la extracción de jugos con un consumo de potencia aceptable y que permita moler cañas adicionales que lleguen al trapiche sin ningún inconveniente o retrasos.
+
+    .col-md-6.mx-auto.mb-5
+      figure
+        .titulo-sexto.color-acento-contenido
+          p.mb-0 
+            p.mb-1 <b>Figura 4.</b><i> Factores a considerar en la determinación de la capacidad del molino</i>
+        img(src='@/assets/curso/temas/T1/img-22.png', alt='Texto que describa la imagen')
+
+
+    ol.lista-ol--cuadro.mb-3
+      li
+        .lista-ol--cuadro__vineta.listas-primera 
+          span 
+        p <b>Mantenimiento y limpieza</b>
+
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-md-5(data-aos="fade-right")
+        img(src='@/assets/curso/temas/T1/img-23.png', alt='mujer con un laptop y un robot')
+      .col-md-7
+        p.mb-3 En las empresas agroindustriales, como los trapiches, pueden surgir dificultades que se convierten en grandes conflictos en el momento de la producción y, por tanto, pérdidas económicas. La mayoría de los problemas se relaciona con la falta de mantenimiento de los equipos, pues su continuo uso genera desgastes en las piezas de manera individual o agrupamiento de ellas; de ahí, que se deba hacer el mantenimiento preventivo -el arreglo o el cambio de alguna pieza- evitando así retrasos en la producción de la panela s y, por ende, disminución en los ingresos económicos para los productores, sus familias y las personas vinculadas con el proceso.
+
+        .cajon.color-secundario.p-4.mb-4.color-cajon
+          p La falta de limpieza de cada equipo no es ajena a los eventos de disminución en la producción y baja en la calidad del producto. El cambio de color o sabor, la presencia de desechos en las panelas o la contaminación cruzada que afecta los jugos, puede acarrear la pérdida del producto. Problemática asociada más a la actividad artesanal y ligada principalmente con la higiene y limpieza del personal manipulador y salubridad.
+
+
+
+    SlyderF.mb-5(columnas="col-lg-6 col-xl-4")
+      .tarjeta.color-acento-botones.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/T1/img-24.svg' alt='AvatarTop')
+        p.text-center Los trapiches deben disponer de manera oportuna con equipos, recipientes y utensilios para garantizar las buenas condiciones sanitarias en la elaboración de la panela, incluyendo los molinos.
+
+      .tarjeta.color-acento-botones.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/T1/img-25.svg' alt='AvatarTop')
+        p.text-center El mantenimiento de los equipos, recipientes y utensilios debe hacerse permanentemente y de forma adecuada para preservar las óptimas condiciones de la producción. 
+
+      .tarjeta.color-acento-botones.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/T1/img-26.svg' alt='AvatarTop')
+        p.text-center El mantenimiento permite reducir las reparaciones que en ocasiones se vuelven cotosas, además de la pérdida de tiempo, el desgaste prematuro de las piezas, los accidentes y la pérdida de caña cortada.
+
+      .tarjeta.color-acento-botones.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/T1/img-27.svg' alt='AvatarTop')
+        p.text-center Durante el mantenimiento se debe acceder a todas las partes del molino y realizar la limpieza de manera adecuada, al igual que la desinfección y el mantenimiento higiénico de los equipos y de las áreas adyacentes.
+
+      .tarjeta.color-acento-botones.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/T1/img-28.svg' alt='AvatarTop')
+        p.text-center La distribución de la planta debe tener un flujo secuencial del proceso de elaboración con el propósito de prevenir la contaminación cruzada, la fermentación de los residuos en los jugos, los malos olores y evitar la presencia de insectos.
 
 
 </template>
